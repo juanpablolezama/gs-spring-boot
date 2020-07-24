dc030598d031d2472fcdf575048fa255e005c662
Juan Pablo Lezama <jlezamaa@everis.com>
2020-07-23 20:01:59 -0500

    Changed to HelloController 22

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot 22!";
     }
     
     @RequestMapping("/hello2")
